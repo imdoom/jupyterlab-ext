b0b333d1d9c358c16f50a3bfb7d0c70f8eaad93b
Akshay Kumar <akumar@esri.com>
2023-05-05 18:43:04 -0700

    Seed apod project from extension template

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+/**
+ * Initialization data for the jupyterlab_apod extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab_apod:plugin',
+  description: 'A JupyterLab extension to show NASA pictures in a panel.',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension jupyterlab_apod is activated!');
+  }
+};
+
+export default plugin;
